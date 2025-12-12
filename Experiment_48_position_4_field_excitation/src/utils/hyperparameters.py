4fc5c7d47cf7a1fdecc91ee24fcb5ea85b0696f9
hernannenjer <159898739+hernannenjer@users.noreply.github.com>
2025-12-12 16:21:51 +0300

    Add files via upload

---
+++ Experiment_48_position_4_field_excitation/src/utils/hyperparameters.py
@@ -0,0 +1,47 @@
+
+from os.path import join
+
+# Extracted defaults from interface.py
+GENERAL_DATAPATH_DEFAULT = join('..', '..', '..', '..', 'Experiments')
+
+# fs_default = 100000         # sampling frequency
+# t1_default = 5e-3          # Width of one impulse in seconds
+# t2_default = 495e-3         # Distance between impulses in seconds
+# dead_time_default = 1.3e-3  # Dead time in seconds
+# period_default = 1000e-3    # Period in seconds (default 1s)
+
+# reg_par_default = 1e-1      # Regularization parameter for ME analysis
+# N_tau_default = 100         # Number of tau points
+# tau_sampling_default = 'non_uniform'  # Tau sampling type
+# tau_min_default = 2e-4      # Minimum tau
+# tau_max_default = 500e-3    # Maximum tau
+
+
+#for 75 sensors
+
+
+# fs_default = 100000         # sampling frequency
+# t1_default = 20e-3          # Width of one impulse in seconds
+# t2_default = 440e-3         # Distance between impulses in seconds
+# dead_time_default = 1.3e-3  # Dead time in seconds
+# period_default = 500e-3    # Period in seconds (default 1s)
+
+# reg_par_default = 1e-1      # Regularization parameter for ME analysis
+# N_tau_default = 100         # Number of tau points
+# tau_sampling_default = 'non_uniform'  # Tau sampling type
+# tau_min_default = 2e-4      # Minimum tau
+# tau_max_default = 500e-3    # Maximum tau
+
+
+fs_default = 100000         # sampling frequency
+t1_default = 5e-3          # Width of one impulse in seconds
+t2_default = 495e-3 #245e-3         # Distance between impulses in seconds
+dead_time_default = 1.3e-3  # Dead time in seconds
+period_default = 500e-3    # Period in seconds (default 1s)
+
+reg_par_default = 1e-1      # Regularization parameter for ME analysis
+N_tau_default = 100         # Number of tau points
+tau_sampling_default = 'non_uniform'  # Tau sampling type
+tau_min_default = 2e-4      # Minimum tau
+tau_max_default = 500e-3    # Maximum tau
+
